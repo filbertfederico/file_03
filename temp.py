e9f0c96cddd4ddaa7cb1322d4401930b227e26ef
filbertfederico <91465463+filbertfederico@users.noreply.github.com>
2021-10-28 10:51:15 +0700

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,16 +1,6 @@
-def convert_temp() :
-    tf = eval(input("Enter a temperature in Fahrenheit:"))
-    print("The temperature in Fahrenheit is:", tf)
-    convert_to_celcius(tf)   
-
-def convert_to_celcius(tf,tc):
-    tc = 5/9(tf-32)
-    print("The temperature in Celsius is:",tc)
-    return    
-
-def convert_to_kelvin(tc,tk):
-    tk = (tc+273.15)
-    print("The temperature in Kelvin is:",tk)
-    
-
-
+def convert_temp():
+    f = int(input("Enter the temperature in Fahrenheit: "))
+    print(f"The temperature in Fahrenheit is: {f}")
+    print(f"The temperature in Celcius is: {(f-32) * 5 / 9}")
+    print(f"The temperature in Kelvin is: {(f-32) * 5 / 9 + 273.15}")
+convert_temp()
