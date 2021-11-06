05e76578939deb56fe744f655505630f80311295
filbertfederico <91465463+filbertfederico@users.noreply.github.com>
2021-11-07 00:53:06 +0700

    moved to repo 2

--- temp.py
+++
@@ -1,6 +0,0 @@
-def convert_temp():
-    f = int(input("Enter the temperature in Fahrenheit: "))
-    print(f"The temperature in Fahrenheit is: {f}")
-    print(f"The temperature in Celcius is: {(f-32) * 5 / 9}")
-    print(f"The temperature in Kelvin is: {(f-32) * 5 / 9 + 273.15}")
-convert_temp()
