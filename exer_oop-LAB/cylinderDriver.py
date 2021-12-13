328b722e302fcc184f42402cc7b445b10de6159e
filbertfederico <91465463+filbertfederico@users.noreply.github.com>
2021-12-13 09:16:00 +0700

    Update cylinderDriver.py

--- exer_oop-LAB/cylinderDriver.py
+++ exer_oop-LAB/cylinderDriver.py
@@ -2,8 +2,6 @@ from cylinder import cylinder
 
 cy = cylinder(1.0, "red", 1.0)
 radius = float(input("input radius: "))
-while radius < 1 : 
-    radius = float(input("input radius value can't be zero, re-input radius :"))
 color = str(input("input color: "))
 height = float(input("input height of the cylinder: ")) 
 
